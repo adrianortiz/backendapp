64baa931c7c7eb4a58241296bf046b20854377e9
Adrian Ortiz Martinez <adrianortizmartinez@gmail.com>
2016-12-22 18:56:50 -0600

    List All courses

--- src/main/java/com/codizer/controller/CourseController.java
+++ src/main/java/com/codizer/controller/CourseController.java
@@ -1,5 +1,7 @@
 package com.codizer.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -18,19 +20,23 @@ public class CourseController {
 	
 	private static final String COURSES_VIEW = "courses";
 	
+	private static final Log LOG = LogFactory.getLog(CourseController.class);
+	
 	@Autowired
 	@Qualifier("courseServiceImpl")
 	private CourseService courseService;
 	
 	@GetMapping("/listcourses")
 	public ModelAndView listAllCourses() {
+		LOG.info("CALL: " + "listAllCourses()");
 		ModelAndView mav = new ModelAndView(COURSES_VIEW);
-		mav.addObject("coruses", courseService.listAllCourses());
+		mav.addObject("courses", courseService.listAllCourses());
 		return mav;
 	}
 	
 	@PostMapping("/addcourse")
 	public String addCourse(@ModelAttribute("course") Course course) {
+		LOG.info("CALL: " + "addCourse() " + " -- Param: " + course.toString());
 		courseService.addCourse(course);
 		return "redirect:/listCourses";
 	}
--- src/main/java/com/codizer/service/impl/CourseServiceImpl.java
+++ src/main/java/com/codizer/service/impl/CourseServiceImpl.java
@@ -2,6 +2,8 @@ package com.codizer.service.impl;
 
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -13,17 +15,21 @@ import com.codizer.service.CourseService;
 @Service("courseServiceImpl")
 public class CourseServiceImpl implements CourseService {
 
+	private static final Log LOG = LogFactory.getLog(CourseServiceImpl.class);
+	
 	@Autowired
 	@Qualifier("courseJpaRepository")
 	private CourseJpaRepository courseJpaRepository;
 	
 	@Override
 	public List<Course> listAllCourses() {
+		LOG.info("CALL: " + "listAllCourses()");
 		return courseJpaRepository.findAll();
 	}
 
 	@Override
 	public Course addCourse(Course course) {
+		LOG.info("CALL: " + "addCourse()");
 		// Devuleve el objeto que acabamos de guardar
 		return courseJpaRepository.save(course);
 	}
